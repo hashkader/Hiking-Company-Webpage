54e32535589ec77b6752c40b50da620c042b6706
Hashim <hashkader07@gmail.com>
2022-08-05 17:08:52 +0200

    initialized git repository

---
+++ js/templatemo-script.js
@@ -0,0 +1,99 @@
+const initBg = (autoplay = true) => {
+    const bgImgsNames = ['background-1.jpg', 'background-2.jpg', 'background-3.jpg'];
+    const bgImgs = bgImgsNames.map(img => "img/" + img);
+
+    $.backstretch(bgImgs, { duration: 4000, fade: 500 });
+
+    if (!autoplay) {
+        $.backstretch('pause');
+    }
+}
+
+const setBg = id => {
+    $.backstretch('show', id);
+}
+
+const setBgOverlay = () => {
+    const windowWidth = window.innerWidth;
+    const bgHeight = $('body').height();
+    const tmBgLeft = $('.tm-bg-left');
+
+    $('.tm-bg').height(bgHeight);
+
+    if (windowWidth > 768) {
+        tmBgLeft.css('border-left', `0`)
+            .css('border-top', `${bgHeight}px solid transparent`);
+    } else {
+        tmBgLeft.css('border-left', `${windowWidth}px solid transparent`)
+            .css('border-top', `0`);
+    }
+}
+
+$(document).ready(function () {
+    const autoplayBg = true;	// set Auto Play for Background Images
+    initBg(autoplayBg);
+    setBgOverlay();
+
+    const bgControl = $('.tm-bg-control');
+    bgControl.click(function () {
+        bgControl.removeClass('active');
+        $(this).addClass('active');
+        const id = $(this).data('id');
+        setBg(id);
+    });
+
+    $(window).on("backstretch.after", function (e, instance, index) {
+        const bgControl = $('.tm-bg-control');
+        bgControl.removeClass('active');
+        const current = $(".tm-bg-controls-wrapper").find(`[data-id=${index}]`);
+        current.addClass('active');
+    });
+
+    $(window).resize(function () {
+        setBgOverlay();
+    });
+});
+
+(function () {
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    //I'm adding this section so I don't have to keep updating this pen every year :-)
+    //remove this if you don't need it
+    let today = new Date(),
+        dd = String(today.getDate()).padStart(2, "0"),
+        mm = String(today.getMonth() + 1).padStart(2, "0"),
+        yyyy = today.getFullYear(),
+        nextYear = yyyy + 1,
+        dayMonth = "07/17/",
+        birthday = dayMonth + yyyy;
+
+    today = mm + "/" + dd + "/" + yyyy;
+    if (today > birthday) {
+        birthday = dayMonth + nextYear;
+    }
+    //end
+
+    const countDown = new Date(birthday).getTime(),
+        x = setInterval(function () {
+
+            const now = new Date().getTime(),
+                distance = countDown - now;
+
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+            //do something later when date is reached
+            if (distance < 0) {
+                document.getElementById("headline").innerText = "Its Game Time!";
+                document.getElementById("countdown").style.display = "none";
+                document.getElementById("content").style.display = "block";
+                clearInterval(x);
+            }
+            //seconds
+        }, 0)
+}());
